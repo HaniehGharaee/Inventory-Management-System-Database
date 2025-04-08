9df19c5138afd1af178be8291c100127b20a1ae5
HaniehGharaee <gharaie.hanieh@gmail.com>
2025-04-08 12:23:10 +0330

    write new query with LIKE, AND, OR,...

--- queries/queries.sql
+++ queries/queries.sql
@@ -19,3 +19,24 @@ ORDER BY price DESC; --ASC|DESC;
 SELECT * FROM products
 ORDER BY name DESC;
 -------------------------------------------------------------------------
+SELECT * FROM products
+WHERE stock_quantity > 12 AND name LIKE 'T%' AND name LIKE '%u';
+--------------------------------------------------------------------------
+SELECT * FROM products
+WHERE  stock_quantity >= 12 AND (name LIKE 'C%' OR name LIKE '%s');
+---------------------------------------------------------------------------
+SELECT * FROM products
+WHERE  stock_quantity >= 12 AND (name LIKE 'C%' OR name LIKE 'K%');
+------------------------------------------------------------------------------------------
+SELECT * FROM products
+WHERE  stock_quantity >= 12 AND name LIKE 'C%' OR name LIKE 'K%';---Without parenthesis,
+------------------------------------------------------------------------------------------
+SELECT * FROM orders
+WHERE NOT order_status = 'Delivered';
+------------------------------------------------------------------------------------------
+SELECT * FROM products
+WHERE stock_quantity > 12 AND name NOT LIKE 'T%' OR name NOT LIKE '%u';
+-------------------------------------------------------------------------------------------
+SELECT * FROM products
+WHERE category_id NOT BETWEEN 1 AND 6; ---between must be number not string;
+--------------------------------------------------------------------------------------------
